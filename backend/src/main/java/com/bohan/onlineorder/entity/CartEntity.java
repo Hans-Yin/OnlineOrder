9c475a132ad1f6ffbd7984cda0d3c39f3f3ffd18
Hans-Yin <tftzseckill@gmail.com>
2026-04-21 17:29:29 -0400

    Add 5 Entities for the DB table mapping

---
+++ backend/src/main/java/com/bohan/onlineorder/entity/CartEntity.java
@@ -0,0 +1,12 @@
+package com.bohan.onlineorder.entity;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+
+@Table("carts")
+public record CartEntity(
+        @Id Long id,
+        Long customerId,
+        Double totalPrice
+) {
+}
